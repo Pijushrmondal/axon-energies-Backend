b8bb409efef90fa3316c7eee177db7867c0926c1
Pijush Ray Mondal <pijush@antino.com>
2026-04-02 11:17:39 +0530

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const swaggerPath = 'api';
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  await app.listen(port);
+
+  const url = await app.getUrl();
+
+  console.log(`App running on: ${url}`);
+  console.log(` Swagger docs: ${url}/${swaggerPath}`);
 }
 bootstrap();
