0425efb28a0edb1f17394ef3b5c9ef1616aeb9c1
Pijush Ray Mondal <pijush@antino.com>
2026-04-03 11:01:40 +0530

    basic logger added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppConfigModule } from './shared/setup/config/config.setup';
 import { AppDatabaseModule } from './shared/setup/database/postgres/database.setup';
+import { LoggerMiddleware } from './shared/infra/middleware/logger.middleware';
 
 @Module({
   imports: [AppConfigModule, AppDatabaseModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
